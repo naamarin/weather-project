7db35b15795f9a22889f4676bdfbcf8be84015f8
Naama Iluz <naamarin@g.jct.ac.il>
2024-09-18 16:27:03 +0300

    Add comments

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
+
 const API_KEY = '8ee633956bad6ae1965b557a94ecfcba'; 
 const cities = [
   { name: 'אילת', id: 'Eilat' },
@@ -13,15 +14,21 @@ const cities = [
 function App() {
   const [weatherData, setWeatherData] = useState({});
 
+  // Fetching Weather Data
   useEffect(() => {
     const fetchWeatherData = async () => {
+      //  loop through each city and fetch its weather data.
       const promises = cities.map(async (city) => {
+        // call the OpenWeatherMap API for each city.
         const response = await axios.get( 
           `http://api.openweathermap.org/data/2.5/weather?q=${city.id}&appid=${API_KEY}&units=metric&lang=he`
         );
+        // The response for each city is stored in a new object with the city name and its weather data.
         return { ...city, data: response.data };
       });
+      // Await for all the API requests to finish.
       const results = await Promise.all(promises);
+      // store the results in the weatherData state as an object where each city is the key.
       const dataMap = results.reduce((acc, city) => {
         acc[city.id] = city.data;
         return acc;
@@ -31,6 +38,8 @@ function App() {
 
     fetchWeatherData();
   }, []);
+
+
   return(<h1>Weather System</h1>);
 }
 
